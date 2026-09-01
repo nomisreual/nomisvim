21d9cf9c19ac8d9f74ee12c1c6ef1944d63f8c56
Simon Antonius Lauer <nomisreual@proton.me>
2026-09-01 10:18:50 +0200

    chore: update

--- config/plugs/treesitter.nix
+++ config/plugs/treesitter.nix
@@ -1,15 +1,15 @@
 {
   plugins.treesitter = {
     enable = true;
+    indent = {
+      enable = true;
+      disable = ["ruby"];
+    };
     settings = {
       auto_install = false;
       highlight = {
         enable = true;
         additional_vim_regex_highlighting = ["ruby"];
-      };
-      indent = {
-        enable = true;
-        disable = ["ruby"];
       };
     };
   };
